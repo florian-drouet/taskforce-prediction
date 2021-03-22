433a52f44fc357445fba2ff09bb2380daddfba74
fdrouetcovidom <80833912+fdrouetcovidom@users.noreply.github.com>
2021-03-22 16:22:47 +0100

    small fixes

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -21,49 +20,49 @@ app = dash.Dash("dashboard_RH", meta_tags=[{"name": "viewport", "content": "widt
 server = app.server
 
 app.layout = html.Div(children=[
-        html.Div(className='grid-container',
-            children=[
-                html.Div(className="header",
-                         children=[
-                             html.Div('Global taskforce prediction with control parameters', className="header--title"),
-                             html.Div('Made with \u2764\ufe0f with Dash.', className="header--subtitle")]),
-                html.Div(className="controllers",
-                         children=[
-                            html.Label("Projection parameter"),
-                            dcc.Input(id='projection_parameter',
-                                    type='number',
-                                    value=1.01,
-                                    step=0.001,
-                                    ),
-                            html.Label("Number of days"),
-                            dcc.Input(id='number_of_days_projections',
-                                    type='number',
-                                    value=60,
-                                    min=1,
-                                    step=1,
-                                    ),
-                            html.Label("Type of projection"),
-                            dcc.Dropdown(id='projection_mode',
-                                        options=[
-                                            {'label': 'Linear', 'value': 'linear'},
-                                            {'label': 'Quadratic', 'value': 'quadratic'},
-                                            {'label': 'Exponential', 'value': 'exponential'}
-                                        ],
-                                        value='quadratic',
-                                        ),
-                            html.Label("Type of population"),
-                            dcc.Dropdown(id='population_type',
-                                        options=[
-                                            {'label': 'ITS', 'value': 'nurse'},
-                                            {'label': 'Doctors', 'value': 'doctor'}
-                                        ],
-                                        value='nurse',
-                                        ),
-                            ]),
-                html.Div(className='fig1', children=[dcc.Graph(id='its_graph')]),
-                html.Div(className='fig2', children=[dcc.Graph(id='alert_graph')])
-                    ])
-        ])
+                html.Div(className='grid-container',
+                    children=[
+                        html.Div(className="header",
+                                children=[
+                                    html.Div('Global taskforce prediction with control parameters', className="header--title"),
+                                    html.Div('Made with \u2764\ufe0f with Dash.', className="header--subtitle")]),
+                        html.Div(className="controllers",
+                                children=[
+                                    html.Label("Projection parameter"),
+                                    dcc.Input(id='projection_parameter',
+                                            type='number',
+                                            value=1.01,
+                                            step=0.001,
+                                            ),
+                                    html.Label("Number of days"),
+                                    dcc.Input(id='number_of_days_projections',
+                                            type='number',
+                                            value=60,
+                                            min=1,
+                                            step=1,
+                                            ),
+                                    html.Label("Type of projection"),
+                                    dcc.Dropdown(id='projection_mode',
+                                                options=[
+                                                    {'label': 'Linear', 'value': 'linear'},
+                                                    {'label': 'Quadratic', 'value': 'quadratic'},
+                                                    {'label': 'Exponential', 'value': 'exponential'}
+                                                ],
+                                                value='quadratic',
+                                                ),
+                                    html.Label("Type of population"),
+                                    dcc.Dropdown(id='population_type',
+                                                options=[
+                                                    {'label': 'ITS', 'value': 'nurse'},
+                                                    {'label': 'Doctors', 'value': 'doctor'}
+                                                ],
+                                                value='nurse',
+                                                ),
+                                    ]),
+                        html.Div(className='fig1', children=[dcc.Graph(id='its_graph')]),
+                        html.Div(className='fig2', children=[dcc.Graph(id='alert_graph')])
+                            ])
+                ])
 
 @app.callback(Output('its_graph', 'figure'),
               Output('alert_graph', 'figure'),
