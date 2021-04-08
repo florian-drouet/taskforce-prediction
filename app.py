67812e0cf8cfa75965ebd0068c58222ac53bb963
Florian Drouet <florian.drouet78@gmail.com>
2021-04-08 17:12:27 +0200

    Black formatting for improving readibility

--- app.py
+++ app.py
@@ -12,146 +12,204 @@ from dash.dependencies import Input, Output
 from tools.func import update_data, get_data, machine_learning_parameters
 from tools.plot import plot_taskforce, plot_alert
 
-if sys.argv[1]=='DEBUG':
-    DEBUG=True
+if sys.argv[1] == "DEBUG":
+    DEBUG = True
 else:
-    DEBUG=False
+    DEBUG = False
 
 X, y = get_data(DEBUG)
 scaler, lin_reg = machine_learning_parameters()
 
-app = dash.Dash("dashboard_RH", meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}])
+app = dash.Dash(
+    "dashboard_RH",
+    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+)
 
 server = app.server
 
-app.layout = html.Div(children=[
-                html.Div(className='grid-container',
+app.layout = html.Div(
+    children=[
+        html.Div(
+            className="grid-container",
+            children=[
+                html.Div(
+                    className="header",
                     children=[
-                        html.Div(className="header",
-                                children=[
-                                    html.Div('Global taskforce prediction with control parameters', className="header--title"),
-                                    html.Div('Made with \u2764\ufe0f with Dash.', className="header--subtitle"),
-                                    html.Img(src='/assets/logo_covidom.png', className='img')]),
-                        html.Div(className="controllers",
-                                children=[
-                                    dcc.DatePickerSingle(id='start_date',
-                                                        date=datetime.date(2020,3,10),
-                                                        display_format='DD/MM/YYYY',
-                                                        min_date_allowed=datetime.date(2020, 3, 10),
-                                                        placeholder='START DATE',
-                                                        ),
-                                    html.Label("Type of population"),
-                                    dcc.Dropdown(id='population_type',
-                                                options=[
-                                                    {'label': 'ITS', 'value': 'nurse'},
-                                                    {'label': 'Doctors', 'value': 'doctor'}
-                                                ],
-                                                value='nurse',
-                                                ),
-                                    html.Label("Type of projection"),
-                                    dcc.Dropdown(id='projection_mode',
-                                                options=[
-                                                    {'label': 'Linear', 'value': 'arithmetic'},
-                                                    {'label': 'Quadratic', 'value': 'geometric'},
-                                                    {'label': 'Bell curve', 'value': 'bell_curve'}
-                                                ],
-                                                value='bell_curve',
-                                                ),
-                                    html.Label("Number of days"),
-                                    dcc.Input(id='number_of_days_projections',
-                                            type='number',
-                                            value=60,
-                                            min=1,
-                                            step=1,
-                                            required=True,
-                                            ),
-                                    html.Label("Arithmetic parameter"),
-                                    dcc.Input(id='arithmetic_parameter',
-                                            type='number',
-                                            value=20,
-                                            step=1,
-                                            disabled=True,
-                                            required=True,
-                                            ),
-                                    html.Label("Geometric parameter"),
-                                    dcc.Input(id='geometric_parameter',
-                                            type='number',
-                                            value=1.01,
-                                            step=0.001,
-                                            disabled=False,
-                                            required=True,
-                                            ),
-                                    html.Label("Alert's peak"),
-                                    dcc.Input(id='alerts_peak',
-                                            type='number',
-                                            value=20,
-                                            min=1,
-                                            step=1,
-                                            disabled=True,
-                                            required=True,
-                                            ),
-                                    html.Label("Upstream slope"),
-                                    dcc.Input(id='coef_bell1',
-                                            type='number',
-                                            value=1.02,
-                                            step=0.001,
-                                            disabled=True,
-                                            required=True,
-                                            ),
-                                    html.Label("Downstream slope"),
-                                    dcc.Input(id='coef_bell2',
-                                            type='number',
-                                            value=0.97,
-                                            step=0.001,
-                                            disabled=True,
-                                            required=True,
-                                            ),
-                                    ]),
-                        html.Div(className='fig1', children=[dcc.Graph(id='its_graph')]),
-                        html.Div(className='fig2', children=[dcc.Graph(id='alert_graph')])
-                            ])
-                ])
+                        html.Div(
+                            "Global taskforce prediction with control parameters",
+                            className="header--title",
+                        ),
+                        html.Div(
+                            "Made with \u2764\ufe0f with Dash.",
+                            className="header--subtitle",
+                        ),
+                        html.Img(src="/assets/logo_covidom.png", className="img"),
+                    ],
+                ),
+                html.Div(
+                    className="controllers",
+                    children=[
+                        dcc.DatePickerSingle(
+                            id="start_date",
+                            date=datetime.date(2020, 3, 10),
+                            display_format="DD/MM/YYYY",
+                            min_date_allowed=datetime.date(2020, 3, 10),
+                            placeholder="START DATE",
+                        ),
+                        html.Label("Type of population"),
+                        dcc.Dropdown(
+                            id="population_type",
+                            options=[
+                                {"label": "ITS", "value": "nurse"},
+                                {"label": "Doctors", "value": "doctor"},
+                            ],
+                            value="nurse",
+                        ),
+                        html.Label("Type of projection"),
+                        dcc.Dropdown(
+                            id="projection_mode",
+                            options=[
+                                {"label": "Linear", "value": "arithmetic"},
+                                {"label": "Quadratic", "value": "geometric"},
+                                {"label": "Bell curve", "value": "bell_curve"},
+                            ],
+                            value="bell_curve",
+                        ),
+                        html.Label("Number of days"),
+                        dcc.Input(
+                            id="number_of_days_projections",
+                            type="number",
+                            value=60,
+                            min=1,
+                            step=1,
+                            required=True,
+                        ),
+                        html.Label("Arithmetic parameter"),
+                        dcc.Input(
+                            id="arithmetic_parameter",
+                            type="number",
+                            value=20,
+                            step=1,
+                            disabled=True,
+                            required=True,
+                        ),
+                        html.Label("Geometric parameter"),
+                        dcc.Input(
+                            id="geometric_parameter",
+                            type="number",
+                            value=1.01,
+                            step=0.001,
+                            disabled=False,
+                            required=True,
+                        ),
+                        html.Label("Alert's peak"),
+                        dcc.Input(
+                            id="alerts_peak",
+                            type="number",
+                            value=20,
+                            min=1,
+                            step=1,
+                            disabled=True,
+                            required=True,
+                        ),
+                        html.Label("Upstream slope"),
+                        dcc.Input(
+                            id="coef_bell1",
+                            type="number",
+                            value=1.02,
+                            step=0.001,
+                            disabled=True,
+                            required=True,
+                        ),
+                        html.Label("Downstream slope"),
+                        dcc.Input(
+                            id="coef_bell2",
+                            type="number",
+                            value=0.97,
+                            step=0.001,
+                            disabled=True,
+                            required=True,
+                        ),
+                    ],
+                ),
+                html.Div(className="fig1", children=[dcc.Graph(id="its_graph")]),
+                html.Div(className="fig2", children=[dcc.Graph(id="alert_graph")]),
+            ],
+        )
+    ]
+)
 
-@app.callback(Output('its_graph', 'figure'),
-              Output('alert_graph', 'figure'),
-              Input('start_date', 'date'),
-              Input('arithmetic_parameter', 'value'),
-              Input('geometric_parameter', 'value'),
-              Input('number_of_days_projections', 'value'),
-              Input('projection_mode', 'value'),
-              Input('population_type', 'value'),
-              Input('alerts_peak', 'value'),
-              Input('coef_bell1', 'value'),
-              Input('coef_bell2', 'value'))
-def update_graphs(start_date, arithmetic_parameter, geometric_parameter, number_of_days_projections, projection_mode, population_type,  alerts_peak, coef_bell1, coef_bell2):
-    '''
+
+@app.callback(
+    Output("its_graph", "figure"),
+    Output("alert_graph", "figure"),
+    Input("start_date", "date"),
+    Input("arithmetic_parameter", "value"),
+    Input("geometric_parameter", "value"),
+    Input("number_of_days_projections", "value"),
+    Input("projection_mode", "value"),
+    Input("population_type", "value"),
+    Input("alerts_peak", "value"),
+    Input("coef_bell1", "value"),
+    Input("coef_bell2", "value"),
+)
+def update_graphs(
+    start_date,
+    arithmetic_parameter,
+    geometric_parameter,
+    number_of_days_projections,
+    projection_mode,
+    population_type,
+    alerts_peak,
+    coef_bell1,
+    coef_bell2,
+):
+    """
     This function updates the two graphs with the parameters from the controller's section.
-    '''
+    """
     # converts start date input into datetime
     start = datetime.datetime.strptime(start_date, "%Y-%m-%d")
     y_plot = y[population_type]
-    
-    if alerts_peak>=number_of_days_projections:
-        raise Exception('WARNING : The peak of alerts cannot be greather than (or equal) to the number of projection days ! Please enter anothe value.')
-    
-    y_true, y_future, alerts = update_data(X=X.loc[X.index>=start], y=y_plot.loc[y_plot.index>=start], model=lin_reg[population_type], preprocessing=scaler, projection_mode=projection_mode, arithmetic_parameter=arithmetic_parameter, geometric_parameter = geometric_parameter, number_of_days_projections=number_of_days_projections,  alerts_peak=alerts_peak, coef_bell1=coef_bell1, coef_bell2=coef_bell2)
+
+    if alerts_peak >= number_of_days_projections:
+        raise Exception(
+            "WARNING : The peak of alerts cannot be greather than (or equal) to the number of projection days ! Please enter anothe value."
+        )
+
+    y_true, y_future, alerts = update_data(
+        X=X.loc[X.index >= start],
+        y=y_plot.loc[y_plot.index >= start],
+        model=lin_reg[population_type],
+        preprocessing=scaler,
+        projection_mode=projection_mode,
+        arithmetic_parameter=arithmetic_parameter,
+        geometric_parameter=geometric_parameter,
+        number_of_days_projections=number_of_days_projections,
+        alerts_peak=alerts_peak,
+        coef_bell1=coef_bell1,
+        coef_bell2=coef_bell2,
+    )
     fig1 = plot_taskforce(y_true, y_future, population_type)
     fig2 = plot_alert(alerts, y_future)
     return fig1, fig2
 
-@app.callback(Output('arithmetic_parameter', 'disabled'),
-              Output('geometric_parameter', 'disabled'),
-              Output('alerts_peak', 'disabled'),
-              Output('coef_bell1', 'disabled'),
-              Output('coef_bell2', 'disabled'),
-              Input('projection_mode', 'value'))
+
+@app.callback(
+    Output("arithmetic_parameter", "disabled"),
+    Output("geometric_parameter", "disabled"),
+    Output("alerts_peak", "disabled"),
+    Output("coef_bell1", "disabled"),
+    Output("coef_bell2", "disabled"),
+    Input("projection_mode", "value"),
+)
 def enable_inputs(value):
-    if value == 'arithmetic':
-        return False,  True, True, True, True
-    elif value == 'geometric':
+    if value == "arithmetic":
+        return False, True, True, True, True
+    elif value == "geometric":
         return True, False, True, True, True
-    elif value == 'bell_curve':
+    elif value == "bell_curve":
         return True, True, False, False, False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True, use_reloader=False)
--- tools/plot.py
+++ tools/plot.py
@@ -1,53 +1,38 @@
 import plotly.graph_objects as go
 
+
 def plot_taskforce(y_true, y_future, population_type):
 
-    pop_translate = {'nurse':'ITS', 'doctor':'Doctor'}
+    pop_translate = {"nurse": "ITS", "doctor": "Doctor"}
 
-    trace_1 = go.Scatter(x=y_true.index,
-                         y=y_true.values,
-                         name="training")
-    trace_2 = go.Scatter(x=y_future.index,
-                         y=y_future.values,
-                         name="predictions")
+    trace_1 = go.Scatter(x=y_true.index, y=y_true.values, name="training")
+    trace_2 = go.Scatter(x=y_future.index, y=y_future.values, name="predictions")
 
     # Create fig and add layout
     layout = go.Layout(
         xaxis=dict(
-            showgrid=False, # Hide Gridlines
-            showline=False, # Hide X-Axis
+            showgrid=False,  # Hide Gridlines
+            showline=False,  # Hide X-Axis
             rangeselector=dict(
-                buttons=list([
-                dict(count=1,
-                     label="1m",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=6,
-                     label="6m",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=1,
-                     label="YTD",
-                     step="year",
-                     stepmode="todate"),
-                dict(count=1,
-                     label="1y",
-                     step="year",
-                     stepmode="backward"),
-                dict(step="all")
-            ])
+                buttons=list(
+                    [
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                        dict(count=1, label="YTD", step="year", stepmode="todate"),
+                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+            rangeslider=dict(visible=True),
+            type="date",
         ),
-        rangeslider=dict(
-            visible=True
-        ),
-        type="date"
-    ),
         yaxis=dict(
-            showgrid=False, # Hide Gridlines
-            showline=False, # Hide X-Axis
-            fixedrange = False
-            #gridcolor='Black',
-            #gridwidth=0.5
+            showgrid=False,  # Hide Gridlines
+            showline=False,  # Hide X-Axis
+            fixedrange=False
+            # gridcolor='Black',
+            # gridwidth=0.5
         ),
     )
 
@@ -58,65 +43,63 @@ def plot_taskforce(y_true, y_future, population_type):
     fig1.add_trace(trace_2)
 
     # Add shape regions
-    fig1.add_vrect(x0=y_future.index[0],
-                   x1=y_future.index[-1],
-                   fillcolor="LightGrey",
-                   opacity=0.5,
-                   layer="below",
-                   line_width=0,
-                   name='prediction period')
+    fig1.add_vrect(
+        x0=y_future.index[0],
+        x1=y_future.index[-1],
+        fillcolor="LightGrey",
+        opacity=0.5,
+        layer="below",
+        line_width=0,
+        name="prediction period",
+    )
 
-    fig1.update_layout(plot_bgcolor='whitesmoke', title=f"{pop_translate[population_type]} taskforce prediction")
-    fig1.update_yaxes(range=(0,max(y_true.max(), y_future.max())))
-    
+    fig1.update_layout(
+        plot_bgcolor="whitesmoke",
+        title=f"{pop_translate[population_type]} taskforce prediction",
+    )
+    fig1.update_yaxes(range=(0, max(y_true.max(), y_future.max())))
+
     return fig1
 
+
 def plot_alert(alerts, y_future):
-    trace_1 = go.Bar(x=alerts.index,
-                     y=alerts.number_of_orange_alerts,
-                     name="orange_alerts",
-                     marker_color='gold')
-    trace_2 = go.Bar(x=alerts.index,
-                     y=alerts.number_of_red_alerts,
-                     name="red_alerts",
-                     marker_color='tomato')
+    trace_1 = go.Bar(
+        x=alerts.index,
+        y=alerts.number_of_orange_alerts,
+        name="orange_alerts",
+        marker_color="gold",
+    )
+    trace_2 = go.Bar(
+        x=alerts.index,
+        y=alerts.number_of_red_alerts,
+        name="red_alerts",
+        marker_color="tomato",
+    )
 
     # Create fig and add layout
     layout = go.Layout(
-        barmode='stack',
+        barmode="stack",
         xaxis=dict(
-            showgrid=False, # Hide Gridlines
-            showline=False, # Hide X-Axis
+            showgrid=False,  # Hide Gridlines
+            showline=False,  # Hide X-Axis
             rangeselector=dict(
-                buttons=list([
-                dict(count=1,
-                     label="1m",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=6,
-                     label="6m",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=1,
-                     label="YTD",
-                     step="year",
-                     stepmode="todate"),
-                dict(count=1,
-                     label="1y",
-                     step="year",
-                     stepmode="backward"),
-                dict(step="all")
-            ])
-        ),
-        rangeslider=dict(
-            visible=True
-        ),
-        type="date"
+                buttons=list(
+                    [
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                        dict(count=1, label="YTD", step="year", stepmode="todate"),
+                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+            rangeslider=dict(visible=True),
+            type="date",
         ),
         yaxis=dict(
-            showgrid=False, # Hide Gridlines
-            showline=False, # Hide X-Axis
-            fixedrange = False  
+            showgrid=False,  # Hide Gridlines
+            showline=False,  # Hide X-Axis
+            fixedrange=False,
         ),
     )
 
@@ -127,14 +110,18 @@ def plot_alert(alerts, y_future):
     fig2.add_trace(trace_2)
 
     # Add shape regions
-    fig2.add_vrect(x0=y_future.index[0],
-                   x1=y_future.index[-1],
-                   fillcolor="LightGrey",
-                   opacity=0.5,
-                   layer="below",
-                   line_width=0,
-                   name='prediction period')
+    fig2.add_vrect(
+        x0=y_future.index[0],
+        x1=y_future.index[-1],
+        fillcolor="LightGrey",
+        opacity=0.5,
+        layer="below",
+        line_width=0,
+        name="prediction period",
+    )
 
-    fig2.update_layout(plot_bgcolor='whitesmoke', title="Evolution of red and orange alerts")
-    
+    fig2.update_layout(
+        plot_bgcolor="whitesmoke", title="Evolution of red and orange alerts"
+    )
+
     return fig2
